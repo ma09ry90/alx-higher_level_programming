5e47e7b8bdd5972b20df2cdad38737a0751f56bf
ma09ry90 <mengistumary60@gmail.com>
2024-04-22 13:10:28 -0700

    input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""  Write a function that appends a string
- at the end of a text file (UTF8) and returns
-  the number of characters added:  """
+"""[Write a function that returns the
+ number of lines of a text file:]"""
 
 
-def append_write(filename="", text=""):
-    """ “Append” mode will add data to the
-     end of the file. Pass mode='a' to the
-      open() function.
+def number_of_lines(filename=""):
+    """[count the number of lines of a text]
 
     Args:
         filename (str, optional): [name of the file to
          open and read]. Defaults to "".
-        text (str, optional): [string to append in the text].
-         Defaults to "".
-
-    Returns:
-        [int]: [number of characters added]
     """
-    with open(filename, mode="a", encoding="UTF8") as MyFile:
-        return (MyFile.write(text))
+    count = 0
+    with open(filename, encoding="UTF8") as MyFile:
+        for line in MyFile:
+            count += 1
+    return(count)
