4a84dbd2c77ef78da91d46b41bbb7a0301be8fb7
ma09ry90 <mengistumary60@gmail.com>
2024-04-22 10:03:47 -0700

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Write a class MyList that inherits from list """
+
+
+class MyList(list):
+    """[Public instance method that prints the list, but sorted (ascending sort)]
+
+    Arguments:
+        list {[Class]} -- [Class list]
+    """
+    def print_sorted(self):
+        print(sorted(self))
