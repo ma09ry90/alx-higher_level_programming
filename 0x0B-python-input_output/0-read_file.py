4f23962ff13abdfee3b92f85ac4b22cf3eee3fc1
ma09ry90 <mengistumary60@gmail.com>
2024-04-22 23:02:08 -0700

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Read file"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """a function that reads a text file (UTF8)
+    and prints it to stdout
+
+    Args:
+        filename(any): name of the file
+
+    """
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
