42eb2cf0d74c0a5fdc2907181e3759516f4062e9
ma09ry90 <mengistumary60@gmail.com>
2024-05-23 12:24:40 -0700

    t-16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -8,12 +8,4 @@ SELECT t.`title`, g.`name`
 
        LEFT JOIN `tv_genres` AS g
        ON s.`genre_id` = g.`id`
- ORDER BY t.`title`, g.`name`;-- Lists all shows and genres linked to the show from the
--- database hbtn_0d_tvshows.
--- Records are ordered by ascending show title and genre name.
-SELECT t.`title`, g.`name`
-  FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-
-       LEFT JOIN i
+ ORDER BY t.`title`, g.`name`;
