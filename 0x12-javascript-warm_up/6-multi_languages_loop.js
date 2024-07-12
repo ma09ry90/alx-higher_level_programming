b088c154b57d862288514a4efbaf24a12cdf79ea
ma09ry90 <mengistumary60@gmail.com>
2024-07-12 13:06:30 -0700

    multi

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
