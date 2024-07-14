9b21d043108ab34650178399ba110117946b205d
ma09ry90 <mengistumary60@gmail.com>
2024-07-14 13:14:25 -0700

    mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+File: 3-my_safe_filter_states.py
+Desc: This module contains a python script that takes in arguments
+and displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    states_info = cur.fetchall()
+
+    for state in states_info:
+        if (state[1] == argv[4]):
+            print(state)
